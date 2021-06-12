77eab20253815320d3305570a853f96b86c6bd94
David Onuh <davidonuh1@gmail.com>
2021-06-12 18:20:29 +0100

    more url rewrite

--- project/views.py
+++ project/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 # Create your views here.
 
@@ -6,10 +7,10 @@ def index(request):
 	return render(request, 'project/index.html')
 
 def dashboard(request):
+	if request.user.is_superuser:
+		return redirect('/admin')
 	return render(request, 'project/dashboard.html')
 
 def plans(request):
 	return render(request, 'project/subscription.html')
-	
-
 	

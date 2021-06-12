06385271e383ff2c677df9bac82b44414ccb7397
David Onuh <davidonuh1@gmail.com>
2021-06-12 18:11:04 +0100

    add models to admin

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from project.models import Plans, Subscription, User
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Plans)
+admin.site.register(Subscription)

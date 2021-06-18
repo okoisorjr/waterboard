28e56ed9e9c233a8ba502befceb445efca64627d
David Onuh <davidonuh1@gmail.com>
2021-06-18 04:09:16 +0100

    custom user admin

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from project.models import Plans, Subscription, User
 # Register your models here.
-admin.site.register(User)
+
+class SubscriptionInline(admin.StackedInline):
+    """ Subscription model inline display for User Model """
+
+    model = Subscription
+    verbose_name_plural = 'subscriptions'
+
+
+class UserAdmin(admin.ModelAdmin):
+    """ Customized User Model display to include user profile, referral, instances and subscripiton """
+
+    list_display = ('username', 'first_name', 'email')
+    inlines = (
+        SubscriptionInline,
+    )
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Plans)
-admin.site.register(Subscription)
